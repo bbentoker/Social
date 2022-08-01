2ea12469f034236bd806e1242099d720e2fb88b3
Toker <bbentoker@gmail.com>
2022-08-01 20:20:27 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/test1',function(){
-    return "test1";
-});
-
-Route::get('/test2',function(){
-    return Inertia::render('Test');
-});
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
